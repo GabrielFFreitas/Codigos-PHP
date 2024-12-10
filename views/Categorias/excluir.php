cf1c15572df794165f225588dd3f3019c5c2a8e0
GabrielFFreitas <129867825+GabrielFFreitas@users.noreply.github.com>
2024-12-10 11:39:12 -0300

    Add files via upload

---
+++ views/Categorias/excluir.php
@@ -0,0 +1,17 @@
+<?php
+
+include "../includes/autoLoad.php";
+Security::verifyAuthentication();
+
+if(isset($_GET['id']) && !empty($_GET['id'])){
+    require_once "../../controllers/CategoriaController.php";
+
+    $CategoriaController = new CategoriaController();
+
+    $res = $CategoriaController->remove($_GET['id']);
+
+    if($res){
+        header("location: ./");
+        exit();
+    }
+}
